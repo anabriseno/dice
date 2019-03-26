23cbf7ab1583fc1e4c5c453d8955f0eccb68b9f8
Student <example@example.com>
2019-03-25 19:49:31 -0700

    Added CSS and HTML and some JS

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// $(document).ready(function() {
+  // $('input[type=button]').click(function)
+    // var = Math.floor(Math.random() * 6);
+
+
+// });
+
+
+$(document).ready(function() {
+  $("form#dice"'input[type=button]').click(function() {
+
+  )}
+)};
