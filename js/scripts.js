28f2676339806c41ebaa14208a67cb234c0389ac
Student <example@example.com>
2019-03-27 20:35:41 -0700

    added more HTML and cleaned up js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,40 @@
-$(document).ready(function() {
+var player1 = function () {
+  return Math.floor(Math.random() * 6) + 1;
 
-  $("form#player1").(function() {
-   event.preventDefault();
-   var player1  = parseInt ($("input#player1").val());
-     return Math.floor(Math.random() * 6) + 1 ;
-  });
+}
 
-  $("form#player2").(function() {
-    event.preventDefault();
-    var player2 = parseInt ($("input#player1").val());
-      return Math.floor(Math.random()* 6) + 1 ;
-  });
+var player2 = function () {
+  return Math.floor(Math.random() * 6) + 1;
+
+}
+
+function player1 (roll1, total1) {
+  this.roll1 = roll1
+  this.total1 = total
+}
+
+function player2 (roll2, total2) {
+  this.roll2 = roll2
+  this.total2 = total2
+}
+
+
+$ (document).ready(function() {
+
+  $("button#start").click(function(event){
+      $(".player-console").hide();
+      clearValues();
+      player1name.newGame();
+      player2name.newGame();
+      $("#round-total-1").empty();
+      $("#total-score-1").empty();
+      $("#die-roll-1").empty();
+      $("#round-total-2").empty();
+      $("#total-score-2").empty();
+      $("#die-roll-2").empty();
+
+      $(".start-menu").show();
+    });
+
 
 });
